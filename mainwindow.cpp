555336f8525e6d7166fbe2b14f0f0c25c333c68f
lvanTheCrazy <70589882+lvanTheCrazy@users.noreply.github.com>
2021-05-06 16:12:36 +0300

    enable_button
    врубает доступ к кнопке при правильных значениях

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,29 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::enable_button1()
+{
+    ui->pushButton->setEnabled(check_N*check_T);
+}
+
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    QString input = arg1;
+    int pos = 0;
+    QIntValidator check(0, 400, this);
+
+    check_N = check.validate(input,pos) == 2; //0 1 2
+    enable_button1();
+}
+
+void MainWindow::on_lineEdit_2_textChanged(const QString &arg1)
+{
+    QString input = arg1;
+    //input.replace(",",".");
+    int pos = 0;
+    QDoubleValidator check(-1000, 400000, 1000, this);
+
+    check_T = check.validate(input,pos) == 2; //0 1 2
+    enable_button1();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
 private:
+    void enable_button1();
+    bool check_N = false;
+    bool check_T = false;
     Ui::MainWindow *ui;
 };
 
